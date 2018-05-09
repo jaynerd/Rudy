174ce3f407befb81695f8ec9a74b807465eb2e76
unknown <j4yn3rd@gmail.com>
2018-05-09 19:23:36 +1200

    wip: intergrating database

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,10 @@
 process.env.DEBUG = 'actions-on-google:*';
 
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+const paperDatabase = admin.database().ref('/paper');
 
 exports.rudy = functions.https.onRequest((request, response) => {
 	
@@ -16,10 +20,16 @@ exports.rudy = functions.https.onRequest((request, response) => {
   }
   
   let requirement1 = '';
+  
   if (request.body.queryResult.parameters['requirement1']){
 	  requirement1 = request.body.queryResult.parameters['requirement1'];
   }
-    response.json({ 'fulfillmentText': requirement }); 
+  
+  paperDatabase.child(paper).once('value', snap => {
+	  const prereq = snap.val().p;
+	      response.json({ 'fulfillmentText': prereq }); 
+  });
+
   // Call get requirement
  // getRequirement(paper, requirement, requirement1).then((output) => {
 //	  response.json({ 'fulfillmentText': output }); // Return the results of the getRequirement function to Dialogflow
@@ -27,4 +37,3 @@ exports.rudy = functions.https.onRequest((request, response) => {
 //	  response.json({ 'fulfillmentText': 'Unable to process your request. Please try with different sentence structures.' });
 //  });
 });
-
